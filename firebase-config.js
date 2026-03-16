bb3006c5cc3f2c07ccd791a3be3c5104045b1d37
DanyLave <7lave.dany11@gmail.com>
2026-03-16 22:07:41 +0100

    Add Firebase Realtime Database multiplayer backend

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Inserisci qui la configurazione del tuo progetto Firebase.
+// La trovi in Firebase Console > Impostazioni progetto > Le tue app > Config SDK.
+window.FIREBASE_CONFIG = {
+    apiKey: "INSERISCI_API_KEY",
+    authDomain: "INSERISCI_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://INSERISCI_PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "INSERISCI_PROJECT_ID",
+    storageBucket: "INSERISCI_PROJECT_ID.appspot.com",
+    messagingSenderId: "INSERISCI_SENDER_ID",
+    appId: "INSERISCI_APP_ID"
+};
